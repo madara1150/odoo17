b1453bfa6144b96d0d6d11f6a0804fa6b7133464
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2024-05-14 19:59:31 +0700

    update : Smart btn

--- addons/practice/models/property.py
+++ addons/practice/models/property.py
@@ -180,6 +180,14 @@ class Property(models.Model):
         action['context'] = {'default_property_id' : self.id}
         return action
     
+    # กดแล้วจะเปิดหน้าแสดงข้อมูล form owner ขึ้นมา
+    def action_open_related_owner(self):
+        action = self.env['ir.actions.actions']._for_xml_id('practice.owner_action')
+        view_id = self.env.ref('practice.owner_view_form').id
+        action['res_id'] = self.owner_id.id
+        action['views'] = [[view_id, 'form']]
+        return action
+    
     
     # เมื่อสร้าง record จะแสดงปริ้น
     # @api.model_create_multi
